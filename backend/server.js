910070097c8d7dcb433e5099c228e103f380ce61
kaziabulhasib <hasibkazi420@gmail.com>
2025-09-17 14:21:39 +0530

    product mode, route and  getallproduct controller created

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ dotenv.config();
 import express from "express";
 import { connectDB } from "./lib/db.js";
 import authRouter from "./routes/auth.route.js";
+
+import productRouter from "./routes/product.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -13,7 +15,9 @@ app.use(cookieParser());
 
 app.use(express.json()); // to parse data from body to json
 
+// routes
 app.use("/api/auth", authRouter);
+app.use("/api/products", productRouter);
 
 app.get("/", (req, res) => res.send("hello"));
 
