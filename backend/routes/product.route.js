e16285602bc48fb21c5a67f1baab0af14879240b
kaziabulhasib <hasibkazi420@gmail.com>
2025-09-18 10:42:59 +0530

    get product by category and recommended product category created

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -6,12 +6,16 @@ import {
   deleteProduct,
   getAllProducts,
   getFeaturedProducts,
+  getProductsByCategory,
+  getRecommendedProducts,
 } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category:category", getProductsByCategory);
+router.get("/recommendation", getRecommendedProducts);
 router.post("/", createProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 export default router;
