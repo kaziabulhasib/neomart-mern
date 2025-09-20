db08763e58b325a3d7215df3673a053420a900e1
kaziabulhasib <hasibkazi420@gmail.com>
2025-09-21 01:01:27 +0530

    navigate to home after login/signu & adding Loading spinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
 import { userUserStore } from "./stores/useUserStore";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 const App = () => {
   const { user, checkAuth, checkingAuth } = userUserStore();
@@ -14,7 +15,7 @@ const App = () => {
     checkAuth();
   }, [checkAuth]);
 
-  if (checkingAuth) return <h1>Checking Auth .....</h1>;
+  if (checkingAuth) return <LoadingSpinner />;
   return (
     <div className='min-h-screen bg-gray-900 text-white relative overflow-hidden '>
       {/* Background gradient */}
@@ -28,8 +29,14 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/login' element={user ? <HomePage /> : <LoginPage />} />
+          <Route
+            path='/signup'
+            element={!user ? <SignUpPage /> : <Navigate to='/' />}
+          />
+          <Route
+            path='/login'
+            element={!user ? <LoginPage /> : <Navigate to='/' />}
+          />
         </Routes>
       </div>
       <Toaster />
