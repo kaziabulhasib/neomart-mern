984129994b751f019272144a06103ff1fb574c01
kaziabulhasib <hasibkazi420@gmail.com>
2025-09-29 00:02:48 +0530

    fixing the catch all 2

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,12 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*/splat", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
